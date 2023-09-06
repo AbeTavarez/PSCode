a711073bba74e672455cb9a8e9a323b3f0ca7652
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-09-06 19:00:45 -0400

    six methods added to the library

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+import concat from './lib/concat';
+import forEach from './lib/forEach';
+import max from './lib/max';
+import min from './lib/min';
+import sum from './lib/sum';
+import reverse from './lib/reverse';
 
 
 /**
@@ -6,7 +11,12 @@
  * @description Small JavaScript utility library
  */
 class PSCode {
-
+    static concat = concat;
+    static forEach = forEach;
+    static max = max;
+    static min = min;
+    static sum = sum;
+    static reverse = reverse;
 }
 
 export default PSCode;
